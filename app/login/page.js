1d97102c7717012fde07b7530c40ef1d13ff8657
Cleign <Ibnukhaidar@gmail.com>
2024-06-04 09:21:34 +0700

    done making login page

---
+++ app/login/page.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+
+const LoginPage = () => {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-rose-100 to-sky-100">
+        <Head>
+          <title>Login Page</title>
+        </Head>
+        <div className="bg-white p-8 rounded-xl shadow-md max-w-md">
+          <h1 className="text-2xl font-medium text-center mb-8">Login</h1>
+          <form>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
+                id="username"
+                type="text"
+                placeholder="Username"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
+                id="password"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+};
+
+export default LoginPage;
--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-rose-100 to-sky-100">
+
     </main>
   );
 }
