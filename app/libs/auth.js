729e44912d852fdd9aa043b29c0f731b3259239c
Cleign <Ibnukhaidar@gmail.com>
2024-06-07 02:32:08 +0700

    sign in backend

---
+++ app/libs/auth.js
@@ -0,0 +1,34 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/app/libs/prisma";
+
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    pages: {
+      signIn: '/login'
+    },
+    providers: [
+      CredentialsProvider({
+        name: "credentials",
+        credentials: {
+          username: { label: "username", type: "text" },
+          password: { label: "Password", type: "password" },
+        },
+        // Stopped at 37:40
+        // https://youtu.be/bicCg4GxOP8?t=2260
+        async authorize(credentials) {
+            if(!credentials?.username || !credentials?.password) {
+                return null;
+            }
+
+            const existingUser = await prisma.user.findUnique({
+                where: { username: credentials?.username },
+            })
+        },
+      }),
+    ],
+    debug: process.env.NODE_ENV === "development",
+    session: { stragegy: "jwt" },
+    secret: "secret"
+  };
--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,37 @@
+'use client';
+
 import Head from "next/head";
-import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 const LoginPage = () => {
+
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  })
+
+  const [alert, setAlert] = useState({
+    status: "",
+    message: "",
+  })
+
+  const onChange = e => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    try {
+      await signIn("credentials", loginData)
+      setAlert({ status: "success", message: "Login successfully" })
+      setLoginData({ email: "", password: "" })
+    } catch (error) {
+      console.log({ error })
+      setAlert({ status: "error", message: "Something went wrong" })
+    }
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-rose-100 to-sky-100">
       <Head>
@@ -9,22 +39,29 @@ const LoginPage = () => {
       </Head>
       <div className="bg-white p-8 rounded-xl shadow-md max-w-md">
         <h1 className="text-2xl font-medium text-center mb-8">Login</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="mb-4">
+            <label htmlFor="username">Username</label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
-              id="username"
+              name="Username"
               type="text"
               placeholder="Username"
+              onChange={onChange}
+              value={loginData.username}
               required
             />
           </div>
           <div className="mb-6">
+            <label htmlFor="password">Kata Sandi</label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
-              id="Password"
-              type="Password"
-              placeholder="Password"
+              name="password"
+              type="password"
+              value={loginData.password}
+              onChange={onChange}
+              placeholder="Kata Sandi"
+              autoComplete="current-password"
               required
             />
           </div>
@@ -33,9 +70,7 @@ const LoginPage = () => {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              <Link href="/siswa">
-                Masuk
-              </Link>
+              Masuk
             </button>
           </div>
         </form>
