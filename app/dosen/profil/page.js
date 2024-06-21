8011fb9cf04a9b92d325e6a2eec280f3e411986d
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-06-21 16:39:01 +0700

    restored

--- app/dosen/profil/page.js
+++ app/dosen/profil/page.js
@@ -4,7 +4,10 @@ export default function DataPresensi() {
   return (
     <div>
       <LayoutDosen>
+        <div className="p-6">
+
         <h1 className="text-black">testing halaman profil dosen</h1>
+        </div>
       </LayoutDosen>
     </div>
   );
