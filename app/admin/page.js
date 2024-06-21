01e6c63c161e7d0fe17d6403e8429fbe841bd0e4
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-06-21 19:16:38 +0700

    added info on admin

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,10 +1,56 @@
+"use client";
+
 import { LayoutAdmin } from "@/components/Sidebar_admin/Layout-Admin";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
 
 export default function DashboardAdmin() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div className="items-center text-center p-96 text-2xl">Loading...</div>
+    );
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <div className="items-center text-center p-96 text-2xl">
+        Not authenticated
+      </div>
+    );
+  }
+
   return (
     <div>
       <LayoutAdmin>
-        <h1 className="text-black">testing halaman data Presensi dosen</h1>
+        <div className="p-6 text-black">
+          <h1 className="text-2xl font-bold mb-8 ">
+            Selamat Datang, {session.user?.username}!
+          </h1>
+          <div className="grid grid-cols-2 gap-4 mb-8">
+            <div className="flex flex-col items-center p-4 bg-gray-100 rounded shadow">
+              <div className="text-4xl mb-2">🎓</div>
+              <div className="text-xl font-semibold">Mata Kuliah</div>
+              <div className="text-2xl mt-2">6</div>
+            </div>
+            <div className="flex flex-col items-center p-4 bg-gray-100 rounded shadow">
+              <div className="text-4xl mb-2">🎓</div>
+              <div className="text-xl font-semibold">Kelas</div>
+              <div className="text-2xl mt-2">1</div>
+            </div>
+            <div className="flex flex-col items-center p-4 bg-gray-100 rounded shadow">
+              <div className="text-4xl mb-2">👥</div>
+              <div className="text-xl font-semibold">Total Siswa</div>
+              <div className="text-2xl mt-2">32</div>
+            </div>
+            <div className="flex flex-col items-center p-4 bg-gray-100 rounded shadow">
+              <div className="text-4xl mb-2">👥</div>
+              <div className="text-xl font-semibold">Total Wajah</div>
+              <div className="text-2xl mt-2">25</div>
+            </div>
+          </div>
+        </div>
       </LayoutAdmin>
     </div>
   );
