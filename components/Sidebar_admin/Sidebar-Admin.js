81c9dea76c650c4686ff6cd622080a6a9e551cde
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-07-10 16:39:02 +0700

    update sidebar admin
    change to datawajah from datakealas

--- components/Sidebar_admin/Sidebar-Admin.js
+++ components/Sidebar_admin/Sidebar-Admin.js
@@ -143,8 +143,8 @@ export const SidebarAdmin = () => {
               <span className="text-xl ml-2">Data Presensi</span>
             </Link>
           </li>
-          {/* <li>
-            <Link href="/admin/datakelas" className="flex items-center justify-center border rounded-lg bg-[#CDE8E5] p-2 hover:bg-[#EEF7FF]">
+          <li>
+            <Link href="/admin/datawajah" className="flex items-center justify-center border rounded-lg bg-[#CDE8E5] p-2 hover:bg-[#EEF7FF]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -159,9 +159,9 @@ export const SidebarAdmin = () => {
                   d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
                 />
               </svg>
-              <span className="text-xl ml-2">Data Kelas</span>
+              <span className="text-xl ml-2">Data Wajah</span>
             </Link>
-          </li> */}
+          </li>
           <li>
             <Link href="/admin/tentang" className="flex items-center justify-center border rounded-lg bg-[#CDE8E5] p-2 hover:bg-[#EEF7FF]">
               <svg
