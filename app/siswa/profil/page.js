ca29853b9791bd284d7fc1020806fa14c096524a
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-07-10 23:49:34 +0700

    update siswa profil

--- app/siswa/profil/page.js
+++ app/siswa/profil/page.js
@@ -189,7 +189,7 @@ export default function Profil() {
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
-                Daftar Wajah
+                Daftar/Memperbaharui Wajah
               </button>
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
