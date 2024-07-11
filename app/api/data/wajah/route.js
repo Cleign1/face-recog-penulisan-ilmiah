91a68064033f06018633f13549dd1843347a7116
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-07-11 22:26:28 +0700

    update api data wajah

--- app/api/data/wajah/route.js
+++ app/api/data/wajah/route.js
@@ -28,3 +28,30 @@ export async function GET(request) {
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
     }
 }
+
+export async function DELETE(request) {
+    try {
+        const { npm } = await request.json();
+
+        if (!npm) {
+            return NextResponse.json({ error: "NPM tidak disertakan" }, { status: 400 });
+        }
+
+        const existingFaceData = await db.faceData.findUnique({
+            where: { npm },
+        });
+
+        if (!existingFaceData) {
+            return NextResponse.json({ error: "Data wajah tidak ditemukan" }, { status: 404 });
+        }
+
+        await db.faceData.delete({
+            where: { npm },
+        });
+
+        return NextResponse.json({ message: "Data wajah berhasil dihapus" });
+    } catch (error) {
+        console.error("Error deleting face data:", error);
+        return NextResponse.json({ error: "Gagal menghapus data wajah: " + error.message }, { status: 500 });
+    }
+}
