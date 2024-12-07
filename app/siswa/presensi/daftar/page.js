f7292e95b2106c2f7d44d199e1a9a61465233ced
Cleign1 <ibnukhaidar@gmail.com>
2024-12-07 17:57:09 +0700

    update background gradient colors in DaftarPresensi component

--- app/siswa/presensi/daftar/page.js
+++ app/siswa/presensi/daftar/page.js
@@ -6,7 +6,7 @@ const FaceRecognition = dynamic(() => import('@/components/FaceRecognition'), {
 
 export default function DaftarPresensi() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-pink-100 flex flex-col items-center justify-center p-6">
+    <div className="min-h-screen bg-gradient-to-r from-[#FFD659] to-[#FFFFFF] flex flex-col items-center justify-center p-6">
       <FaceRecognition />
     </div>
   );
