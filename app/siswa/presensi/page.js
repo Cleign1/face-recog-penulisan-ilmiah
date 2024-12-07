135e6e277e8691bf6fe605d0b7deb72248568618
Cleign1 <ibnukhaidar@gmail.com>
2024-12-07 17:57:04 +0700

    update background gradient colors in PresensiPage component

--- app/siswa/presensi/page.js
+++ app/siswa/presensi/page.js
@@ -6,7 +6,7 @@ const Presensi = dynamic(() => import('@/components/Presensi'), {
 
 export default function PresensiPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-pink-100 flex flex-col items-center justify-center p-6">
+    <div className="min-h-screen bg-gradient-to-r from-[#FFD659] to-[#FFFFFF] flex flex-col items-center justify-center p-6">
       <Presensi />
     </div>
   );
